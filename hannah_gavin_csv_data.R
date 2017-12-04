d0b9e6663af9f2647c820fc3b906ef392ea3a2cf
Hannah Gavin <hjgavin@uw.edu>
2017-12-03 20:09:59 -0800

    Updated column names

--- hannah_gavin_csv_data.R
+++ hannah_gavin_csv_data.R
@@ -9,18 +9,18 @@ neighborhood.details <- read.csv('Neighborhood_Details-modified.csv', stringsAsF
 #do we want to override the existing data frame or create a new one?
 bike.neighborhood <- group_by(bike.racks, Neighborhood)%>%
   summarise(
-    count = n()
+    "Number of Bike Racks" = n()
   )
 
 #gives you the number of food banks in each neighborhood
 food.bank.neighborhood <- group_by(food.banks, Neighborhood)%>%
   summarise(
-    num.food.banks <- n()
+    "Number of Food Banks" = n()
   )
 
 #provides the number of city features in the dataset 
 sum.city.feature <- group_by(neighborhood.details, City.Feature)%>%
   summarise(
-    count.feat = n()
+    "Number of City Feature" = n()
   )  
 
