bf4c7b1fdaf77b5234254470ecf5d611fe24ce20
Hannah Gavin <hjgavin@uw.edu>
2017-12-03 18:33:27 -0800

    summary information for assigned csv files

--- hannah_gavin_csv_data.R
+++ hannah_gavin_csv_data.R
@@ -2,6 +2,9 @@ library("dplyr")
 
 #read in CSV files:
 bike.racks <- read.csv('City_of_Seattle_Bicycle_Racks-modified.csv', stringsAsFactors=FALSE)
+food.banks <- read.csv('Food_Banks-modified.csv', stringsAsFactors=FALSE)
+neighborhood.details <- read.csv('Neighborhood_Details-modified.csv', stringsAsFactors=FALSE)
+
 #provides the count(number) of neighborhoods 
 #do we want to override the existing data frame or create a new one?
 bike.neighborhood <- group_by(bike.racks, Neighborhood)%>%
@@ -9,15 +12,12 @@ bike.neighborhood <- group_by(bike.racks, Neighborhood)%>%
     count = n()
   )
 
-food.banks <- read.csv('Food_Banks-modified.csv', stringsAsFactors=FALSE)
 #gives you the number of food banks in each neighborhood
 food.bank.neighborhood <- group_by(food.banks, Neighborhood)%>%
   summarise(
     num.food.banks <- n()
   )
 
-  
-neighborhood.details <- read.csv('Neighborhood_Details-modified.csv', stringsAsFactors=FALSE)
 #provides the number of city features in the dataset 
 sum.city.feature <- group_by(neighborhood.details, City.Feature)%>%
   summarise(
