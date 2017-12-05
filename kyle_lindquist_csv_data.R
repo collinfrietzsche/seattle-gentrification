ba7c7cd2aec3454844be8fc21a0fa83fa146592a
Kyle Lindquist <kslindqu@gmail.com>
2017-12-05 11:43:08 -0800

    add police incidents summary

--- kyle_lindquist_csv_data.R
+++ kyle_lindquist_csv_data.R
@@ -4,6 +4,7 @@ library("dplyr")
 garages.and.parking.lots <- read.csv('./data_modified/Public_Garage_or_Parking_Lot-modified.csv', stringsAsFactors = FALSE)
 tennis.courts <- read.csv('./data_modified/Tennis_Courts-modified.csv', stringsAsFactors = FALSE)
 public.spaces <- read.csv('./data_modified/Public_Spaces-modified.csv', stringsAsFactors = FALSE)
+police.incidents <- read.csv('./data_modified/Police_Incidents-modified.csv', stringsAsFactors = FALSE)
 
 # provides num of garages/parking lots by neighborhood
 garages.parking.by.neighborhood <- group_by(garages.and.parking.lots, Neighborhood) %>%
@@ -22,3 +23,10 @@ public.spaces.by.neighborhood <- group_by(public.spaces, Neighborhood) %>%
   summarise(
     "Number of Public Spaces" = n()
   )
+
+# provides num of police incidents by type of offense
+police.incidents.by.offense <- group_by(police.incidents, Summarized.Offense.Description) %>%
+  summarise(
+    count = n()
+  ) %>%
+  arrange(-count)
